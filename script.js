63f7d9dfc4fc5dffe6493ae45e2bbe132a6ba0c9
Elucode <125608503+Elulove1@users.noreply.github.com>
2024-06-09 22:48:02 -0400

    requirements added
    UserName, Email and Password requirements added.

--- script.js
+++ script.js
@@ -16,12 +16,66 @@
 // Registration Form Element
 
 function ValidateRegistration() {
-    const usrname =document.forms["registration"]["username"].value.trim();
+    const usrName =document.forms["registration"]["username"].value.trim();
     const email = document.forms["registration"]["email"].value.trim().toLowerCase();
     const password = document.forms["registration"]["password"].value;
     const passwordCheck = document.forms["registration"]["passwordCheck"].value;
     const terms = document.forms["registration"]["terms"].checked;
 
     clearError();
+
+    if (!userName) {
+        displayError("Username cannot be blank.");
+        return false;
+    }
+
+    if (!isValidEmail(email)) {
+        displayError("Username cannot be blank.");
+        return false;
+    }
+
+    if (!isValidEmail(email)) {
+        displayError("Please enter a valid email address.");
+        return false;
+    }
+
+    if (email.endsWith("@example.com")) {
+        displayError("Email cannot be from the domain 'example.com'.");
+        return false;
+    }
+
+    if (password.length < 12) {
+        displayError("Password must be at least 12 characters long.");
+        return false;
+    }
+
+    if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
+        displayError("Password must contain at least one uppercase and one lowercase letter.");
+        return false;
+    }
+
+    if (!/\d/.test(password)) {
+        displayError("Password must contain at least one number.");
+        return false;
+    }
+
+    if (!/[^a-zA-Z0-9\s]/.test(password)) {
+        displayError("Password must contain at least one special character.");
+        return false;
+    }
+
+    if (password.toLowerCase().includes("password")) {
+        displayError("Password cannot contain the word 'password'.");
+        return false;
+    }
+
+    if (password !== passwordCheck) {
+        displayError ("password do not match.");
+        return false;
+    }
     
+    if (!terms) {
+        displayError("You must agree to the Terms of Use.");
+        return false;
+    }
 }
